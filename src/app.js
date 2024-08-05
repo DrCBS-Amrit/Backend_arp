d2140ee0d39e7cb1232863f30c34f39bf855eee4
DrCBS-Amrit <amritanshu@drcbscyber.com>
2024-08-05 15:52:18 +0530

    inital commit

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const vulnerabilityRoutes = require('./routes/vulnerabilityRoutes');
+const cors=require('cors')
+
+
+
+const app = express();
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/public', express.static(path.join(__dirname, '../public')));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use('/', vulnerabilityRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
